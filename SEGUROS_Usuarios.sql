b369441d7d6b0116d30295f9c82f277b7bcc16e4
David <davidmartinez.alum@insestatut.cat>
2026-05-16 17:22:29 +0200

    cambiado para compatibilidad con linux

--- SEGUROS_Usuarios.sql
+++ SEGUROS_Usuarios.sql
@@ -1,12 +1,12 @@
-use seguros;
+use SEGUROS;
 CREATE USER 'rese'@'localhost' IDENTIFIED BY 'rese';
-GRANT SELECT ON seguros.* TO 'rese'@'localhost';
+GRANT SELECT ON SEGUROS.* TO 'rese'@'localhost';
 
 CREATE USER 'kadha'@'localhost' IDENTIFIED BY 'kadha';
-GRANT SELECT ON seguros.* TO 'kadha'@'localhost';
+GRANT SELECT ON SEGUROS.* TO 'kadha'@'localhost';
 
 CREATE USER 'mecanico'@'localhost' IDENTIFIED BY 'mecanico';
-GRANT SELECT ON seguros.personas TO 'mecanico'@'localhost';
-GRANT SELECT ON seguros.taller TO 'mecanico'@'localhost';
-GRANT SELECT ON seguros.mecanico TO 'mecanico'@'localhost';
-GRANT SELECT ON seguros.mecanico_repara TO 'mecanico'@'localhost';
+GRANT SELECT ON SEGUROS.personas TO 'mecanico'@'localhost';
+GRANT SELECT ON SEGUROS.taller TO 'mecanico'@'localhost';
+GRANT SELECT ON SEGUROS.mecanico TO 'mecanico'@'localhost';
+GRANT SELECT ON SEGUROS.mecanico_repara TO 'mecanico'@'localhost';
